24943f5fa799adedef60bb8b063be286be0e0f77
Hackbright Student <info@hackbrightacademy.com>
2014-02-10 16:00:02 -0800

    sorted count and in alphabetical rorder

--- wordcount.py
+++ wordcount.py
@@ -1,36 +1,68 @@
 # opens a file named on the command line 
-"""
-import argv and define arguments 
-make a string out of the imported file
-"""
+# import argv and define arguments 
+# make a string out of the imported file
+
 
 from sys import argv
 script, filename = argv
 
-thefile = open(filename)
+f = open(filename)
+thefile = f.read()
+f.close()
 
-
-
-# thefile = f.read()
-# f.close()
 
 #counts how many times each space-separated word occurs in that file. 
-"""
-    check to see if the word exists in the dictionary
-    if so- add 1 to the counter for that word 
-    if not create a new item with that word as key (counter starts at 1)
-"""
+# check to see if the word exists in the dictionary
+# if so- add 1 to the counter for that word 
+# if not create a new item with that word as key (counter starts at 1)
+
 dictionary = {}
 
-for line in thefile:
-    # print line
-    for word in line.split(" "):
-        word = word.strip()
-        if not dictionary.get(word):
-            dictionary[word] = 1
-        else:
-            dictionary[word] += 1
+# alphabet: ascii 97 through 122
+
+notalphabet = []
+for i in range(0, 97):
+    thechar = chr(i)
+    notalphabet.append(thechar)
+for i in range(123, 128):
+    thechar = chr(i)
+    notalphabet.append(thechar)
+notalphabet = ''.join(notalphabet)
+
+
+for word in thefile.split():
+    word = word.lower()
+    word = word.strip(notalphabet)
+    if not dictionary.get(word):
+        dictionary[word] = 1
+    else:
+        dictionary[word] += 1
+
+## This prints out the word counts with no sorting
+# for word, count in dictionary.iteritems():
+#     print word + " " + str(count)
+
+
+## This will sort them according to the number of appearances
+
+
+# loop through sorted list of keys: get length of its value, sort value list,
+# for i in range(len(value)), print that key along with its value[i] each time
+
+sorteddictionary = {}
 
 for word, count in dictionary.iteritems():
-    print word + " " + str(count)
+    if not sorteddictionary.get(count):
+        sorteddictionary[count] = [word]
+    else:
+        sorteddictionary.get(count).append(word)
+
+key_list = sorteddictionary.keys()
+key_list.sort(reverse = True)
+
+for count in key_list:
+    howmany = len(sorteddictionary[count])
+    sorteddictionary[count].sort()
+    for i in range(howmany):
+        print str(count) + " " + sorteddictionary[count][i]
 
